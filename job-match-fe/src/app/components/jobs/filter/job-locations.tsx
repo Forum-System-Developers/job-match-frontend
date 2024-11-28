147f1489ab2c0dac379e54250c76277497f55206
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-11-28 18:09:13 +0200

    [FEAT] Refactor job data handling to use custom hook for fetching ads

--- job-match-fe/src/app/components/jobs/filter/job-locations.tsx
+++ job-match-fe/src/app/components/jobs/filter/job-locations.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import slugify from 'slugify';
-import job_data from '@/data/job-data';
-import NiceSelect from '@/ui/nice-select';
+import React from "react";
+import slugify from "slugify";
+import NiceSelect from "@/ui/nice-select";
 import { useAppDispatch } from "@/redux/hook";
-import { setLocation } from '@/redux/features/filterSlice';
+import { setLocation } from "@/redux/features/filterSlice";
+import { useAds } from "../../company/hooks/useAds";
 
 const JobLocations = () => {
-  const uniqueLocations = [...new Set(job_data.map(job => job.location))];
+  const { ads, loading: companyLoading } = useAds();
+
+  const uniqueLocations = [...new Set(ads.map((job) => job.location))];
   const dispatch = useAppDispatch();
-  const handleLocation = (item: { value: string; label: string }) => { 
-    dispatch(setLocation(item.value))
+  const handleLocation = (item: { value: string; label: string }) => {
+    dispatch(setLocation(item.value));
   };
   const options = uniqueLocations.map((l) => {
-    return {value:slugify(l.split(',').join('-').toLowerCase(),'-'),label:l}
-  })
+    return {
+      value: slugify(l.split(",").join("-").toLowerCase(), "-"),
+      label: l,
+    };
+  });
   return (
     <NiceSelect
       options={options}
