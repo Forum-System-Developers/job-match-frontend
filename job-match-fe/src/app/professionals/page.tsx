7fed2d9ced78b2d048ceddc76bc6d39530445747
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-12-01 00:21:47 +0200

    [REFACTOR] Update page metadata

--- job-match-fe/src/app/professionals/page.tsx
+++ job-match-fe/src/app/professionals/page.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { Metadata } from 'next';
-import Header from '@/layouts/headers/header';
-import Wrapper from '@/layouts/wrapper';
-import JobBreadcrumb from '../components/jobs/breadcrumb/job-breadcrumb';
-import JobPortalIntro from '../components/job-portal-intro/job-portal-intro';
-import CandidateV1Area from '../components/candidate/candidate-v1-area';
-import FooterOne from '@/layouts/footers/footer-one';
+import React from "react";
+import { Metadata } from "next";
+import Header from "@/layouts/headers/header";
+import Wrapper from "@/layouts/wrapper";
+import JobBreadcrumb from "../components/jobs/breadcrumb/job-breadcrumb";
+import JobPortalIntro from "../components/job-portal-intro/job-portal-intro";
+import CandidateV1Area from "../components/candidate/candidate-v1-area";
+import FooterOne from "@/layouts/footers/footer-one";
 
 export const metadata: Metadata = {
-  title: "Candidate v1",
+  title: "All Professionals",
 };
-
 
 const CandidatePage = () => {
   return (
@@ -21,11 +20,14 @@ const CandidatePage = () => {
         {/* header end */}
 
         {/* search breadcrumb start */}
-        <JobBreadcrumb title='Candidates' subtitle='Find you desire talents & make your work done' />
+        <JobBreadcrumb
+          title="Candidates"
+          subtitle="Find you desire talents & make your work done"
+        />
         {/* search breadcrumb end */}
 
         {/* candidate area start */}
-        <CandidateV1Area/>
+        <CandidateV1Area />
         {/* candidate area end */}
 
         {/* job portal intro start */}
