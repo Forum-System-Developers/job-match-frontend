1bc81859ef1ce7990bdd2df69bea9db6a7cfb459
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-12-03 17:09:09 +0200

    [FEAT] Update user role retrieval and add logout functionality

--- job-match-fe/src/utils/auth_utils.ts
+++ job-match-fe/src/utils/auth_utils.ts
@@ -1,13 +1,10 @@
 import SERVER_URL from "@/services/server";
 import axiosInstance from "@/services/axiosInstance";
-import { CompanyDetails } from "@/app/components/dashboard/employ/data/company-data";
-import { ProfessionalDetails } from "@/app/components/dashboard/candidate/data/professional-data";
-import { use } from "react";
 
 export const setRole = async () => {
   if (typeof window !== "undefined") {
     try {
-      const user = await currentUser();
+      const user = await getUser();
       localStorage.setItem("role", user.role);
     } catch (error) {
       console.error("An error occurred:", error);
@@ -33,12 +30,24 @@ export interface UserDetails {
   role: string;
 }
 
+const getUser = async (): Promise<UserDetails> => {
+  try {
+    const response = await axiosInstance.get(`http://${SERVER_URL}/auth/me`);
+    const userId = response.data.detail.id;
+    const role: string = response.data.detail.role;
+    return { id: userId, role: role };
+  } catch (error) {
+    console.log(error);
+    return { id: "", role: "" };
+  }
+};
+
 export const currentUser = async (): Promise<UserDetails> => {
   if (typeof window !== "undefined" && isAuthenticated()) {
     try {
       const response = await axiosInstance.get(`http://${SERVER_URL}/auth/me`);
       const userId = response.data.detail.id;
-      const role = response.data.detail.role;
+      const role: string = response.data.detail.role;
       return { id: userId, role: role };
     } catch (error) {
       console.error("An error occurred:", error);
@@ -46,4 +55,14 @@ export const currentUser = async (): Promise<UserDetails> => {
   }
   window.location.href = "/";
   return { id: "", role: "" };
+};
+
+export const handleLogout = async () => {
+  try {
+    await axiosInstance.post(`http://${SERVER_URL}/auth/logout`);
+    localStorage.removeItem("role");
+    window.location.href = "/";
+  } catch (error) {
+    console.error("Logout failed:", error);
+  }
 };
