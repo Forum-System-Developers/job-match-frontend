a15a609b4cab79a743cfdecc3c7ba7d78aff2257
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-17 16:57:44 +0200

    [FEAT] Swap localStorage with sessionStorage utility functions for managing session data

--- job-match-fe/src/utils/sessionStorage.ts
+++ job-match-fe/src/utils/sessionStorage.ts
@@ -1,14 +1,14 @@
 import { JobAdResponse } from "@/data/job-ad-data";
 
-export const setLocalStorage = (name: string, item: any) => {
+export const setSessionStorage = (name: string, item: any) => {
   if (typeof window !== "undefined" && typeof localStorage !== "undefined") {
-    localStorage.setItem(name, item);
+    sessionStorage.setItem(name, item);
   }
 };
 
-export const getLocalStorage = (name: string) => {
+export const getSessionStorage = (name: string) => {
   if (typeof window !== "undefined" && typeof localStorage !== "undefined") {
-    const data = localStorage.getItem(name);
+    const data = sessionStorage.getItem(name);
     if (data) {
       return data;
     } else {
@@ -18,8 +18,8 @@ export const getLocalStorage = (name: string) => {
   return null;
 };
 
-export const removeLocalStorage = (name: string) => {
+export const removeSessionStorage = (name: string) => {
   if (typeof window !== "undefined" && typeof localStorage !== "undefined") {
-    localStorage.removeItem(name);
+    sessionStorage.removeItem(name);
   }
 };
