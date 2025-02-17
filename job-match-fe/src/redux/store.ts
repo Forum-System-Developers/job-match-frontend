23150e3bdaba6cfde5c2db0cf6c8fefd85e63675
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-17 17:01:36 +0200

    [REFACTOR] Remove WishlistArea component and related Redux state management

--- job-match-fe/src/redux/store.ts
+++ job-match-fe/src/redux/store.ts
@@ -1,13 +1,11 @@
-import wishlistSlice from './features/wishlist';
-import { configureStore } from '@reduxjs/toolkit';
-import filterSlice from './features/filterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import filterSlice from "./features/filterSlice";
 
 export const store = configureStore({
   reducer: {
-    filter:filterSlice,
-    wishlist:wishlistSlice
+    filter: filterSlice,
   },
-})
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
